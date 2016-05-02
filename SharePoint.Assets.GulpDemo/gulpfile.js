2cfb46eb4e549d7a2a051d7fd532247806f69c31
vman <vardhaman.rd@gmail.com>
2016-05-02 22:03:17 +0100

    updated comments

--- SharePoint.Assets.GulpDemo/gulpfile.js
+++ SharePoint.Assets.GulpDemo/gulpfile.js
@@ -1,21 +1,20 @@
-﻿/*
-This file in the main entry point for defining Gulp tasks and using Gulp plugins.
-Click here to learn more. http://go.microsoft.com/fwlink/?LinkId=518007
-*/
-
-//Load gulp plugin
+﻿//Load gulp
 var gulp = require('gulp');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
-var spsave = require('gulp-spsave');
 
+//Load gulp plugins
+var concat = require('gulp-concat'); //bundle
+var uglify = require('gulp-uglify'); //minify
+var rename = require('gulp-rename'); //renaming to min
+var spsave = require('gulp-spsave'); //upload to SharePoint
+
+//Bundle JS files together into a single file
 gulp.task('concat-js', function () {
     gulp.src("./Scripts/**/*.js")
     .pipe(concat("GulpDemo.js"))
     .pipe(gulp.dest("./Output"))
 });
 
+//Minify the GulpDemo.js file. This task has a dependency on concat-js which means concat-js will always run before minify-js
 gulp.task('minify-js', ['concat-js'], function () {
     gulp.src("./Output/GulpDemo.js")
     .pipe(uglify())
@@ -23,16 +22,19 @@ gulp.task('minify-js', ['concat-js'], function () {
     .pipe(gulp.dest("./Output"))
 });
 
+//Upload GulpDemo.js and GulpDemo.min. jsfile. This task has a dependency on minify-js which means minify-js will always run before upload-to-sp
 gulp.task('upload-to-sp', ['minify-js'], function () {
     gulp.src("./Output/*.js")
       .pipe(spsave({
           username: "dev@yourtenant.onmicrosoft.com",
           password: "Password",
           siteUrl: "https://yourtenant.sharepoint.com/sites/dev/",
-          folder: "Style Library/My Folder"
+          folder: "Style Library/My Folder",
+          checkin: true
       }));
 });
 
+//Watcher task. This will watch for any changes in the scenario1.js and scenario2.js files and then run the upload-to-sp task.
 gulp.task('watch-js-upload-to-sp', function () {
     gulp.watch("./Scripts/**/*.js", ['upload-to-sp']);
 });
